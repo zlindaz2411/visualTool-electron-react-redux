088a57a90aae0e0f4671b2ffa06f4fc6618a3276
zlindaz2411 <linda.zhou.pro@gmail.com>
2020-03-24 15:23:51 +0000

    Add cross env

--- webpack.js
+++ webpack.js
@@ -24,6 +24,7 @@ module.exports = {
                 exclude: '/node_modules/',
             },
             
+            
             {
                 test: /\.(png|jpg|gif)$/,
                 use: [
